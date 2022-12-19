cfaf3d9300989bb297fbe4571733dc30fe384c6d
DebabrataSaha-570 <sahadebabrata570@gmail.com>
2022-12-19 19:57:39 +0600

    react router basic structure add

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home/Home/Home";
+import NotFound from "./Pages/NotFound/NotFound";
+import MyOrders from "./Pages/MyOrders/MyOrders";
+import ManageOrders from "./Pages/ManageOrders/ManageOrders";
+import AddService from "./Pages/AddService/AddService";
+import ContactUs from "./Pages/ContactUs/ContactUs";
 
 function App() {
   return (
     <div>
-      <h1>Trip to paradise</h1>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/home" element={<Home></Home>}></Route>
+        <Route path="/myOrders" element={<MyOrders></MyOrders>}></Route>
+        <Route
+          path="/manageOrders"
+          element={<ManageOrders></ManageOrders>}
+        ></Route>
+        <Route path="/addService" element={<AddService></AddService>}></Route>
+        <Route path="/contactUs" element={<ContactUs></ContactUs>}></Route>
+        <Route path="*" element={<NotFound></NotFound>}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/Pages/NotFound/NotFound.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div>
+      <h3>Page not found!!</h3>
+      <h4>404, Error!</h4>
+    </div>
+  );
+};
+
+export default NotFound;
