a1ba94ea5887f042941c63454f28ad7651e0fd29
DebabrataSaha-570 <sahadebabrata570@gmail.com>
2023-01-20 17:55:03 +0600

    update

--- src/Pages/LogIn/LogIn.js
+++ src/Pages/LogIn/LogIn.js
@@ -2,6 +2,7 @@ import React from "react";
 import Footer from "../Shared/Footer/Footer";
 import Navigation from "../Shared/Navigation/Navigation";
 import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
 
 const LogIn = () => {
   const {
@@ -18,72 +19,78 @@ const LogIn = () => {
   return (
     <>
       <Navigation></Navigation>
-      <div class="max-w-lg mx-auto my-10 bg-white p-8 rounded-xl shadow shadow-slate-300">
-        <h1 class="text-4xl font-medium">Login</h1>
-        <p class="text-slate-500">Hi, Welcome back 👋</p>
+      <div className="max-w-lg mx-auto my-10 bg-white p-8 rounded-xl shadow shadow-slate-300">
+        <h1 className="text-4xl font-medium">Login</h1>
+        <p className="text-slate-500">Hi, Welcome back 👋</p>
 
-        <div class="my-5">
-          <button class="w-full text-center py-3 my-3 border flex space-x-2 items-center justify-center border-slate-200 rounded-lg text-slate-700 hover:border-slate-400 hover:text-slate-900 hover:shadow transition duration-150">
+        <div className="my-5">
+          <button className="w-full text-center py-3 my-3 border flex space-x-2 items-center justify-center border-slate-200 rounded-lg text-slate-700 hover:border-slate-400 hover:text-slate-900 hover:shadow transition duration-150">
             <img
               src="https://www.svgrepo.com/show/355037/google.svg"
-              class="w-6 h-6"
+              className="w-6 h-6"
               alt=""
             />{" "}
             <span>Login with Google</span>
           </button>
         </div>
-        <form action="" class="my-10" onSubmit={handleSubmit(handleLogin)}>
-          <div class="flex flex-col space-y-5">
+        <form action="" className="my-10" onSubmit={handleSubmit(handleLogin)}>
+          <div className="flex flex-col space-y-5">
             <label for="email">
-              <p class="font-medium text-slate-700 pb-2">Email address</p>
+              <p className="font-medium text-slate-700 pb-2">Email address</p>
               <input
-                {...register("email", { required: true })}
+                {...register("email", { required: "Email is required" })}
                 id="email"
                 name="email"
                 type="email"
-                class="w-full py-3 border border-slate-200 rounded-lg px-3 focus:outline-none focus:border-slate-500 hover:shadow"
+                className="w-full py-3 border border-slate-200 rounded-lg px-3 focus:outline-none focus:border-slate-500 hover:shadow"
               />
               {errors.email && (
-                <span className="text-red-500">This field is required</span>
+                <span className="text-red-500">{errors.email?.message}</span>
               )}
             </label>
             <label for="password">
-              <p class="font-medium text-slate-700 pb-2">Password</p>
+              <p className="font-medium text-slate-700 pb-2">Password</p>
               <input
-                {...register("password", { required: true })}
+                {...register("password", {
+                  required: "password is required",
+                  minLength: {
+                    value: 6,
+                    message: "Password must be 6 characters or longer",
+                  },
+                })}
                 id="password"
                 name="password"
                 type="password"
-                class="w-full py-3 border border-slate-200 rounded-lg px-3 focus:outline-none focus:border-slate-500 hover:shadow"
+                className="w-full py-3 border border-slate-200 rounded-lg px-3 focus:outline-none focus:border-slate-500 hover:shadow"
               />
               {errors.password && (
-                <span className="text-red-500">This field is required</span>
+                <span className="text-red-500">{errors.password?.message}</span>
               )}
             </label>
-            <div class="flex flex-row justify-between">
+            <div className="flex flex-row justify-between">
               <div>
-                <label for="remember" class="">
+                <label for="remember" className="">
                   <input
                     type="checkbox"
                     id="remember"
-                    class="w-4 mx-2 h-4 border-slate-200 focus:bg-indigo-600"
+                    className="w-4 mx-2 h-4 border-slate-200 focus:bg-indigo-600"
                   />
                   Remember me
                 </label>
               </div>
               <div>
-                <a href="/" class="font-medium text-indigo-600">
+                <a href="/" className="font-medium text-indigo-600">
                   Forgot Password?
                 </a>
               </div>
             </div>
             <button
               type="submit"
-              class="w-full py-3 font-medium text-white bg-indigo-600 hover:bg-indigo-500 rounded-lg border-indigo-500 hover:shadow inline-flex space-x-2 items-center justify-center"
+              className="w-full py-3 font-medium text-white bg-indigo-600 hover:bg-indigo-500 rounded-lg border-indigo-500 hover:shadow inline-flex space-x-2 items-center justify-center"
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                class="h-6 w-6"
+                className="h-6 w-6"
                 fill="none"
                 viewBox="0 0 24 24"
                 stroke="currentColor"
@@ -97,17 +104,17 @@ const LogIn = () => {
               </svg>
               <span>Login</span>
             </button>
-            <p class="text-center">
+            <p className="text-center">
               Not registered yet?{" "}
-              <a
-                href="/"
-                class="text-indigo-600 font-medium inline-flex space-x-1 items-center"
+              <Link
+                to="/signUp"
+                className="text-indigo-600 font-medium inline-flex space-x-1 items-center"
               >
-                <span>Register now </span>
+                <span>Please Register</span>
                 <span>
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
-                    class="h-4 w-4"
+                    className="h-4 w-4"
                     fill="none"
                     viewBox="0 0 24 24"
                     stroke="currentColor"
@@ -120,7 +127,7 @@ const LogIn = () => {
                     />
                   </svg>
                 </span>
-              </a>
+              </Link>
             </p>
           </div>
         </form>
